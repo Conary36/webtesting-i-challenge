0412f3cbb763b927030ad62d9e26df6931094f90
Conary36 <cbeckford144@gmail.com>
2020-04-30 16:29:09 -0400

    initial commit

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,10 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe("enhancer", ()=>{
+    describe("repair", () =>{
+        it.todo("returns a new item with the durability restored to 100", ()=>{
+            
+        })
+
+    })
+})
