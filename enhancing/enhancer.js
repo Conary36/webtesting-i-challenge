fc42ab8a2dbc9cceb92afa534fd81608a1a5a66c
Conary36 <cbeckford144@gmail.com>
2020-04-30 21:50:30 -0400

    succeed, repair enhancers initiated

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,12 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+const success = item.enhancement + 1;
+if(item.enhancement === 20){
+  return item
+}else{
+  return { ...item, enhancement: success};
+}
 }
 
 function fail(item) {
@@ -14,7 +19,7 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100};
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,10 +1,40 @@
-const enhancer = require('./enhancer.js');
+const {repair, succeed, fail} = require('./enhancer.js');
+
 // test away!
+const weapon = {
+    name: 'excalibre',
+    enhancement: 20,
+    durability: 75
+}
+const Magic = {
+    name: 'fireball',
+    enhancement: 45,
+    durability: 3
+}
+
 describe("enhancer", ()=>{
     describe("repair", () =>{
-        it.todo("returns a new item with the durability restored to 100", ()=>{
-            
+        it("returns a new item with the durability restored to 100", ()=>{
+            expect(repair(weapon)).toEqual({...weapon, durability: 100})  
         })
+        it("returns a new item with the durability restored to 100", () => {
+          expect(repair(weapon)).not.toEqual({ ...weapon, durability: 100 });
+        });
+        
 
     })
+    describe("success", ()=>{
+        it( "returns a new item object modified according to the rules defined by the client for enhancement success.", () =>{
+
+            expect(succeed(weapon)).toEqual({ ...weapon, enhancement: 20 });
+        })  
+        it("returns a new item object modified according to the rules defined by the client for enhancement success.", () => {
+          expect(succeed(weapon)).not.toEqual({ ...weapon, enhancement: 20 });
+        });  
+        // it("increases in power by 1", () =>{
+        //     expect
+        // }
+
+    })
+        
 })
